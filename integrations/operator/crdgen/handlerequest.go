05a2edaed3444c0bc3f69f7d8f0a5f5c78d2bf11
Carlos Castro <carlos.castro@jumo.world>
2025-01-16 14:13:32 +0000

    Teleport Kubernetes Operator supports Bot resource

--- integrations/operator/crdgen/handlerequest.go
+++ integrations/operator/crdgen/handlerequest.go
@@ -178,6 +178,7 @@ func generateSchema(file *File, groupName string, format crdFormatFunc, resp *go
 		{name: "RoleV6", opts: []resourceSchemaOption{withVersionOverride(types.V7), withVersionInKindOverride()}},
 		{name: "SAMLConnectorV2"},
 		{name: "OIDCConnectorV3"},
+		{name: "Bot", opts: []resourceSchemaOption{withVersionInKindOverride()}},
 		{name: "GithubConnectorV3"},
 		{
 			name: "LoginRule",
